5bf793a7e4515d6dd7c86133e962ddd580289c4f
poussiere <von.linnasta@gmail.com>
2016-10-03 00:06:40 +0200

    c

--- app/src/main/java/com/poussiere_violette/poussieremagique/MainActivity.java
+++ app/src/main/java/com/poussiere_violette/poussieremagique/MainActivity.java
@@ -40,27 +40,13 @@ import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
+
 public class MainActivity extends Activity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
+
     private static final boolean AUTO_HIDE = true;
 
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
     private static final int AUTO_HIDE_DELAY_MILLIS= 3000;
 
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
     private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler();
     private View  conteneur;
@@ -72,11 +58,7 @@ public class MainActivity extends Activity {
         @SuppressLint("InlinedApi")
         @Override
         public void run() {
-            // Delayed removal of status and navigation bar
 
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
             mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -88,12 +70,7 @@ public class MainActivity extends Activity {
     private final Runnable mShowPart2Runnable = new Runnable() {
         @Override
         public void run() {
-            // Delayed display of UI elements
-          //  ActionBar actionBar = getSupportActionBar();
-           // if (actionBar != null) {
-            //    actionBar.show();
-          //  }
-          //  mControlsView.setVisibility(View.VISIBLE);
+
         }
     };
     private boolean mVisible;
@@ -103,11 +80,7 @@ public class MainActivity extends Activity {
             hide();
         }
     };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
+
     private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -169,6 +142,7 @@ public class MainActivity extends Activity {
            @Override
            public void onClick(View view) {
 
+               mContentView.setOnClickListener(null);
                conteneur.startAnimation(animation4);
                mContentView.startAnimation(animationSet);
 
@@ -223,17 +197,6 @@ public class MainActivity extends Activity {
 
 
 
-
-       /* // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
-
-       ;
-       */
     }
 
     @Override
@@ -256,14 +219,9 @@ public class MainActivity extends Activity {
     }
 
     private void hide() {
-        // Hide UI first
-     /* ActionBar actionBar = getSupportActionBar();
-       if (actionBar != null) {
-          actionBar.hide();
-        }*/
+
         mVisible = false;
 
-        // Schedule a runnable to remove the status and navigation bar after a delay
         mHideHandler.removeCallbacks(mShowPart2Runnable);
 
       mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
@@ -272,12 +230,12 @@ public class MainActivity extends Activity {
 
     @SuppressLint("InlinedApi")
     private void show() {
-        // Show the system bar
+
         mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
         mVisible = true;
 
-        // Schedule a runnable to display UI elements after a delay
+
         mHideHandler.removeCallbacks(mHidePart2Runnable);
         mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
 
@@ -285,10 +243,6 @@ public class MainActivity extends Activity {
     }
 
 
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
     private void delayedHide(int delayMillis) {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
